9ca8f098daf37c0b5d024ee3d569cb88b39912bd
Andy <linuxarm521@163.com>
2016-05-22 10:10:30 +0800

    add a c file for test
    first test c program

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  
+  while(1)
+  {
+    for(i=0;i<1000;i++)
+    {
+      printf("this is a test program : %d \n",i);
+      sleep(1);
+    }
+  }
+  
+  return 0;
+}
